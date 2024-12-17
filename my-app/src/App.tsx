ea542e5e5515b14bd727989487b9194c93f6eb09
Ebenezer <ebenezeroforia08@gmail.com>
2024-12-17 23:32:07 +0000

    feat: Basic initial screen setup

---
+++ my-app/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header className="welcome">
+      WELCOME
+      </header>
+      <div className="body">
+        <div className="body-top">
+          <div></div>
+          <div className="add-new">
+          Add New Password
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
